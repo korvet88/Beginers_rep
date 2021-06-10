19a02b9bc6873bba6be96287c0a2ef7477446508
korvet88 <regim88@yandex.ru>
2021-06-10 22:11:57 +0700

    cipher_cezar(10.06.21).py
    Добавил: ё  Ё     для алфавита.
    добавил работу сдвига в формулу через step_for_indx.
    Символы добавляются в строку и на вывод подается именно строка.

--- cipher _cezar(04.06.21).py
+++ cipher _cezar(04.06.21).py
@@ -38,6 +38,12 @@ while True:
     if counter == 5:
         print("Иди остынь и вводи верно!")
         exit(0)
+        
+if step_for == "left":
+    step_for_indx = (-1)
+if step_for == "rigth":
+    step_for_indx = (1)
+    
 while True:
     """Fix me для разных яз."""
     print("Введите шаг числом от 0 до 33 для rus и от 0 до 22 для en:")
@@ -57,21 +63,21 @@ print("Вввод даннызх завершен")
 
 eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
 eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
-rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+rus_lower_alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+rus_upper_alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
-out_text = []
-if languadge == "rus":
+out_text = ""
+if languadge == "rus" :
     for i in text:
         if i in rus_upper_alphabet:
-            rez = int(rus_upper_alphabet.find(i) + step%len(rus_upper_alphabet))
-            out_text.append(rus_upper_alphabet(rez))
-        if i in rus_lower_alphabet:
-            rez = int(rus_lower_alphabet.find(i) + step%len(rus_lower_alphabet))
-            out_text.append(rus_lower_alphabet(rez))
+            rez = int(rus_upper_alphabet.find(i) + step_for_indx*(step%len(rus_upper_alphabet)))
+            out_text += rus_upper_alphabet[rez]
+        elif i in rus_lower_alphabet:
+            rez = int(rus_lower_alphabet.find(i) + step_for_indx*(step%len(rus_lower_alphabet)))
+            out_text += rus_lower_alphabet[rez]
         else:
-            out_text.append(i)
-print(*out_text)
+            out_text += i
+print(out_text)
 
 
 
